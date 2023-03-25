9726e1f02f520e335c1dc3a5adea022a67ebcdb6
Jamie Stark <76487361+iamjamiestark@users.noreply.github.com>
2023-03-25 23:38:06 +0000

    Challenge finished

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import qrCode from './images/image-qr-code.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="card">
+          <img src={qrCode} className="qrCode" width="100%" height="100%" alt="QR Code" />
+          <h2 className="titleText">Improve your front-end skills by building projects</h2>
+          <h4 className="subTitleText">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h4>
+        </section>
       </header>
     </div>
   );

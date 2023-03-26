2ab31e0d46caceaa8617faae07145ac0cea2f60e
Jamie Stark <76487361+iamjamiestark@users.noreply.github.com>
2023-03-26 00:13:24 +0000

    Image height/width update to css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <section className="card">
-          <img src={qrCode} className="qrCode" width="100%" height="100%" alt="QR Code" />
+          <img src={qrCode} className="qrCode" alt="QR Code" />
           <h2 className="titleText">Improve your front-end skills by building projects</h2>
           <p className="subTitleText">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </section>

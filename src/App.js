28ac65fe3475c44a7b7a00905370b864d08d9d6a
Jamie Stark <76487361+iamjamiestark@users.noreply.github.com>
2023-03-26 00:11:46 +0000

    Paragraph update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <section className="card">
           <img src={qrCode} className="qrCode" width="100%" height="100%" alt="QR Code" />
           <h2 className="titleText">Improve your front-end skills by building projects</h2>
-          <h4 className="subTitleText">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</h4>
+          <p className="subTitleText">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </section>
       </header>
     </div>
